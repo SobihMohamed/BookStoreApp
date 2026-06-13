f36526a72172eea55b4968d5c99c0f5c0d9311fb
Sobih <sobihmohamedsobih@gmail.com>
2026-06-13 16:22:46 +0300

    implement classic pure OOP Strategy Pattern Created IBookRuleStrategy interface as the base contract. Implemented Concrete Strategies: PercentageDiscount, FlatAmountDiscount, and VatTax.

---
+++ Services/StrategyDP/IBookRuleStrategy.cs
@@ -0,0 +1,12 @@
+﻿using BookStoreConsole.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStoreConsole.Services.Strategies
+{
+    public interface IBookRuleStrategy
+    {
+        void ApplyRule(Book book);
+    }
+}
