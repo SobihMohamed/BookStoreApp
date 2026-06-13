c02075122cb77df71f75683de035c9e01319cfd7
Sobih <sobihmohamedsobih@gmail.com>
2026-06-13 15:44:23 +0300

    implement smart Factory Pattern with DTO and validation" -m " - Created BookFactory using Dictionary and Delegates to adhere to the Open/Closed Principle (OCP)." -m " - Introduced CreateBookDto to resolve long parameter list code smell." -m " - Added Guard Clauses to prevent invalid domain states during book creation.

---
+++ Services/FactoryDP/BookFactory.cs
@@ -0,0 +1,54 @@
+﻿using BookStoreConsole.Domain.Entities;
+using BookStoreConsole.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStoreConsole.Services.FactoryDP
+{
+    public class BookFactory
+    {
+        private Dictionary<string, Func<Book>> _bookCreators = new(StringComparer.OrdinalIgnoreCase);
+        public BookFactory()
+        {
+            RegisterBookType("Paperback", () => new Paperback());
+            RegisterBookType("E-Book", () => new EBook());
+        }
+        public void RegisterBookType(string bookType, Func<Book> creator)
+        {
+            _bookCreators[bookType] = creator;
+        }
+        public Book CreateBook(CreateBookDto dto)
+        {
+            ValidateBookDto(dto);
+
+            // Creation Logic
+            if (_bookCreators.TryGetValue(dto.Format, out var creator))
+            {
+                var book = creator();
+                book.Title = dto.Title;
+                book.Author = dto.Author;
+                book.Category = dto.Category;
+                book.Price = dto.Price;
+                book.Stock = dto.Stock;
+
+                return book;
+            }
+
+            throw new ArgumentException($"Book format '{dto.Format}' is not supported.");
+        }
+        private void ValidateBookDto(CreateBookDto dto)
+        {
+            if (string.IsNullOrWhiteSpace(dto.Title))
+                throw new ArgumentException("Title cannot be empty.");
+            if (string.IsNullOrWhiteSpace(dto.Author))
+                throw new ArgumentException("Author cannot be empty.");
+            if (string.IsNullOrWhiteSpace(dto.Category))
+                throw new ArgumentException("Category cannot be empty.");
+            if (dto.Price <= 0)
+                throw new ArgumentException("Price must be greater than zero.");
+            if (dto.Stock < 0)
+                throw new ArgumentException("Stock cannot be negative.");
+        }
+    }
+}
